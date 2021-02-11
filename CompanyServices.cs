68aabc7caa792251fbddeac9e9d73b7ec8c0405c
Gökhan <gokhanmerden@gmail.com>
2021-02-11 15:37:06 +0300

    asd

--- CompanyServices.cs
+++ CompanyServices.cs
@@ -13,7 +13,37 @@ namespace Ishop.Core.Finance.Services
         FinanceUnitOfWork _financeUnitOfWork;
         public CompanyServices(IConfiguration config, FinanceAppSettings financeAppSettings){
             _financeUnitOfWork = new FinanceUnitOfWork(config);
-        } 
+        }
+
+        public async Task<CombineCompanyResultModel> combineCompanies(CombineCompanyRequestModel model)
+        {
+            CombineCompanyResultModel resultModel = new CombineCompanyResultModel();
+            try
+            {
+                foreach (var companyId in model.sources)
+            {
+                var paymentVochers = await _financeUnitOfWork.PaymentVoucherRepository.GetListAsync(p=>p.companyNo == companyId, true);
+                foreach (var paymentVocher in paymentVochers)
+                {
+                    paymentVocher.companyNo = model.target;
+                    _financeUnitOfWork.PaymentVoucherRepository.Update(paymentVocher);
+                }
+                var company = _financeUnitOfWork.CompanyRepository.GetByID(companyId);
+                company.isDeleted = true;
+                _financeUnitOfWork.CompanyRepository.Update(company);
+                await _financeUnitOfWork.SaveAync();
+                resultModel.result = true;
+                resultModel.message = "Firma birleştirme işlemi tamamlandı";
+            }
+            }
+            catch (System.Exception ex)
+            {
+                resultModel.result = false;
+                resultModel.message = ex.Message;
+            }
+            return resultModel;
+        }
+
         public async Task<PaginatedList<CompanyEntity>> getCompanies(CompanyRequestModel model)
         {
            IEnumerable<CompanyEntity> result = await _financeUnitOfWork.CompanyRepository.GetListAsync<CompanyEntity>(selector: s=> new CompanyEntity(s.id,s.firmaNo,s.firmaAd,s.vergiNo),predicate: p=> (p.firmaAd.Contains(model.name) || p.vergiNo.Contains(model.vergiNo)) && p.isDeleted != true);
--- interfaces/ICompanyServices.cs
+++ interfaces/ICompanyServices.cs
@@ -7,5 +7,6 @@ namespace Ishop.Core.Finance.Services
 {
     public interface ICompanyServices {
         Task<Gokhan.Core.Services.PaginatedList<CompanyEntity>> getCompanies(CompanyRequestModel model); 
+        Task<CombineCompanyResultModel> combineCompanies(CombineCompanyRequestModel model);
     } 
 }
